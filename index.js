62b78f3b6b1a6f9806ba98d3b6606f1c6c124836
brave <progpavlovmark@gmail.com>
2023-03-15 16:29:08 +0300

    dz-22 1.0

---
+++ index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function sum (num1, num2){
+    let result = num1 + num2;
+    return result;
+}
+function sub (num1, num2){
+    let result = num1 - num2;
+    return result;
+}
+function mul (num1, num2){
+    let result = num1 * num2;
+    return result;
+}
+console.log(sum, sub, mul);
